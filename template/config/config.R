57d0fe305ad6de48ac607a178b9623f600b3a621
Robert Flight <r.flight79@gmail.com>
2013-09-06 11:27:52 -0400

    first commit of the blog

---
+++ template/config/config.R
@@ -0,0 +1,17 @@
+####### Samatha static site generator configuration file  ######
+
+###### RSS generation ######
+rss.title <- "Deciphering life: One bit at a time"
+domain <- "https://rmflight.github.io"
+rss.description <- "Robert M Flight's personal blog"
+rss.categories <- list("R", "data", "bioinformatics", "open science")
+# rss.category.feeds <- c("R")
+
+###### tag setup ######
+
+tag.layout = "default_template.R"
+
+###### blog posts ######
+
+post.layout = "default_template.R"
+figure.path = "img"
