600d57a46538dcc1499c4934fe3d1e45abcaf7bd
Robert Flight <r.flight79@gmail.com>
2013-09-11 12:40:32 -0400

    modified index to generate a link to the actual post with the post date

--- template/pages/index.R
+++ template/pages/index.R
@@ -3,18 +3,21 @@ title <- "home"
 
 currState <- get.site.state(site)
 
-postlist <- list.files(file.path(site, "template/posts"))
-postlist <- postlist[str_detect(postlist, "\\.Rmd")]
-postmodified <- file.states(file.path(site, "template", "posts", postlist))
-postdates <- as.Date(str_extract(postlist, "[[:digit:]]{4}_[[:digit:]]{2}_[[:digit:]]{2}"), format = "%Y_%m_%d")
+source_posts <- names(currState$source_posts)
+postdates <- as.Date(str_extract(source_posts, "[[:digit:]]{4}_[[:digit:]]{2}_[[:digit:]]{2}"), format = "%Y_%m_%d")
+postmodified <- currState$source_posts
 
 useorder <- order(postdates, postmodified, decreasing=T)
-postlist <- postlist[useorder]
+
+postmd <- str_replace(source_posts, "\\.Rmd", "\\.md")
+postmd <- postmd[useorder]
 postdates <- postdates[useorder]
 
-curr.post <- str_replace(postlist[1], "\\.Rmd", "\\.md")
+print(postmd[1])
 
-page <- content(include.markdown(file.path(site, "template", "posts", curr.post)),
-  m("h2", "Older Posts:"),
+page <- content(include.markdown(postmd[1]),
+                m("h6",
+                  link.to(get.postpath(postmd[1]), paste0("Posted on ", postdates[1]))),
+                m("h2", "Older Posts:"),
                html.postlist(site))
  
